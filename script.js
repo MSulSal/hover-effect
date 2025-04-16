20f14b562ab6ec8a47fb2c8e0afb6e557b8d9224
MSulSal <MSulSal@users.noreply.github.com>
2025-04-16 20:54:28 +0000

    deploy: b79416ea70fa4691f2425dc041b644d5f122d41b

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-document.getElementById("cards").onmousemove = (e) => {
-  for (const card of document.getElementsByClassName("card")) {
-    const rect = card.getBoundingClientRect(),
-      x = e.clientX - rect.left,
-      y = e.clientY - rect.top;
+const cardsContainer = document.getElementById("cards");
 
-    card.style.setProperty("--mouse-x", `${x}px`);
-    card.style.setProperty("--mouse-y", `${y}px`);
+const updateMousePosition = (x, y) => {
+  for (const card of document.getElementsByClassName("card")) {
+    const rect = card.getBoundingClientRect();
+    const posX = x - rect.left;
+    const posY = y - rect.top;
+    card.style.setProperty("--mouse-x", `${posX}px`);
+    card.style.setProperty("--mouse-y", `${posY}px`);
   }
+};
+
+// Mouse move event for desktop devices
+cardsContainer.onmousemove = (e) => {
+  updateMousePosition(e.clientX, e.clientY);
+};
+
+// Touch move event for mobile devices
+cardsContainer.ontouchmove = (e) => {
+  // Prevent default scrolling behavior when swiping on cards,
+  // but you may choose to remove this if you want scrolling to occur while touching.
+  // e.preventDefault();
+  const touch = e.touches[0];
+  updateMousePosition(touch.clientX, touch.clientY);
 };
