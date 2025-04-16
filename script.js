2a45185ee205a0635a10a1f195a8f83262c36df4
MSulSal <129910316+MSulSal@users.noreply.github.com>
2025-04-16 11:01:25 -0500

    add card content

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-for (const card of document.querySelectorAll(".card")) {
+const handleOnMouseMove = (e) => {
+  const { currentTarget: target } = e;
+
+  const rect = target.getBoundingClientRect(),
+    x = e.clientX - rect.left,
+    y = e.clientY - rect.top;
+
+  target.style.setProperty("--mouse-x", `${x}px`);
+  target.style.setProperty("--mouse-y", `${y}px`);
+};
+
+for (const card of document.getElementsByClassName("card")) {
+  card.onmousemove = (e) => handleOnMouseMove(e);
 }
